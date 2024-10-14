bb702609029485f01a6327202005a48a3d9f1650
CoeJoder <CoeJoder@users.noreply.github.com>
2024-10-14 05:16:22 -0700

    FIx paths and validation for run-deposit-cli script

--- src/run-staking-deposit-cli.sh
+++ src/run-staking-deposit-cli.sh
@@ -53,24 +53,36 @@ check_file_exists --sudo deposit_cli_sha256
 
 print_failed_checks --error || exit 1
 
+validator_keys_parent_dir="$this_dir"
+validator_keys_dir="$validator_keys_parent_dir/validator_keys"
+
+# if validator_keys directory already exists, confirm overwrite
+reset_checks
+check_directory_does_not_exist --sudo validator_keys_dir
+if ! print_failed_checks --warn; then
+	continue_or_exit 1 "Overwrite?"
+	printf '\n'
+fi
+
 # -------------------------- RECONNAISSANCE -----------------------------------
 
-printf '\n'
 printf '%s' \
 	"Using an online PC, navigate to " \
 	"${color_blue}https://github.com/ethereum/staking-deposit-cli/releases/tag/${ethereum_staking_deposit_cli_version}${color_reset} " \
 	"and verify the ${color_lightgray}SHA256 Checksum${color_reset} of ${theme_filename}$deposit_cli_basename${color_reset}"
 printf '\n'
-if ! yes_or_no --default-no "Does it match this? ${theme_value}$ethereum_staking_deposit_cli_sha256_checksum${color_lightgray}"; then
+if ! yes_or_no --default-no "Does it match this? ${theme_value}$ethereum_staking_deposit_cli_sha256_checksum${color_reset}"; then
 	printerr "unexpected checksum; ensure that ${color_lightgray}ethereum_staking_deposit_cli_${color_reset} values in ${theme_filename}env.sh${color_reset} are correct and relaunch this script"
 	exit 1
 fi
+printf '\n'
 
 read_default "Number of validator keys to generate" 1 num_validators
-if [[ ! $num_validators =~ ^[123456789]+$ ]]; then
+if [[ ! $num_validators =~ ^[[:digit:]]+$ || ! $num_validators -gt 0 ]]; then
 	printerr "must choose a positive integer"
 	exit 1
 fi
+printf '\n'
 
 # -------------------------- EXECUTION ----------------------------------------
 
@@ -78,54 +90,57 @@ trap 'on_err_noretry' ERR
 
 assert_sudo
 
-# change to same directory as the tarball and this script
+# change to the dist directory and chown it
 cd "$this_dir"
+sudo chown -R "$USER:$USER" ./
 
 # checksum using the included .sha256 file
+printinfo "Verifying SHA256 checksum..."
 sha256sum -c "$deposit_cli_sha256" # trapped
+printf '\n'
 
 # chown the tarball, unpack it
-sudo chown -v "$USER:$USER" "$deposit_cli_basename"
+printinfo "Unpacking tarball..."
 sudo tar xvzf "$deposit_cli_basename"
+printf '\n'
+
 deposit_cli_dir="${deposit_cli_basename%%.*}"
 deposit_cli_bin="$deposit_cli_dir/deposit"
 
 # assert that all is well so far
 reset_checks
-check_directory_exists --sudo "$deposit_cli_dir"
-check_executable_exists --sudo "$deposit_cli_bin"
+check_directory_exists --sudo deposit_cli_dir
+check_executable_exists --sudo deposit_cli_bin
 print_failed_checks --error # trapped
-
-# chown to current user so we can run without root perms
-sudo chown -R "${USER}:${USER}" "$deposit_cli_dir"
 
 # confirmation message
 cat <<EOF
-Ready to run the following command:
-${color_lightgray}
-$deposit_cli_bin new-mnemonic \\
+Ready to run the following command:${color_lightgray}
+$deposit_cli_bin --language=English new-mnemonic \\
 	--num_validators=$num_validators \\
-	--mnemonic_language=english \\
-	--chain=$ethereum_network
+	--mnemonic_language=English \\
+	--chain="$ethereum_network" \\
+	--folder="$validator_keys_parent_dir"
 ${color_reset}
 EOF
 continue_or_exit 1
 
+# remove any existing keystore
+rm -rf "$validator_keys_dir" &>/dev/null
+
 # generate the key(s)
-$deposit_cli_bin new-mnemonic \
+$deposit_cli_bin --language=English new-mnemonic \
 	--num_validators=$num_validators \
-	--mnemonic_language=english \
-	--chain=$ethereum_network
+	--mnemonic_language=English \
+	--chain="$ethereum_network" \
+	--folder="$validator_keys_parent_dir"
 
 # -------------------------- POSTCONDITIONS -----------------------------------
 
 reset_checks
-validator_keys="$deposit_cli_dir/validator_keys"
-check_directory_exists --sudo validator_keys
+check_directory_exists --sudo validator_keys_dir
 print_failed_checks --error
 
 cat <<EOF
-
-Success!  Generated $num_validators validator keys.
 Now you are ready to import your validator keys to the node server.
 EOF
