f2a856dfc4522c703f561aa8059459b62087d3ba
CoeJoder <CoeJoder@users.noreply.github.com>
2024-10-23 00:29:37 -0700

    Move interpolated SSH command to proper server-side script

---
+++ src/get-validator-statuses.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# get-validator-statuses.sh
+#
+# Queries the status of imported validator accounts.
+# Typically called from the client PC's `export.sh` script, but can be run
+# directly.
+
+# -------------------------- HEADER -------------------------------------------
+
+set -e
+this_dir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+source "$this_dir/common.sh"
+housekeeping
+
+function _show_usage() {
+	cat >&2 <<-EOF 
+	usage:
+	$(basename ${BASH_SOURCE[0]}) outfile
+	EOF
+}
+
+if [[ $# -ne 1 ]]; then
+	_show_usage
+	exit 1
+fi
+_outfile="$1"
+
+# -------------------------- BANNER -------------------------------------------
+
+# -------------------------- PREAMBLE -----------------------------------------
+
+# -------------------------- PRECONDITIONS ------------------------------------
+
+assert_on_node_server
+assert_sudo
+
+check_is_defined ethereum_network
+check_is_defined prysm_validator_wallet_dir
+check_is_defined prysm_validator_wallet_password_file
+check_is_defined prysm_validator_user
+
+for command in tee awk curl jq; do
+	check_command_exists_on_path command
+done
+
+check_executable_exists --sudo prysm_validator_bin
+
+print_failed_checks --error || exit
+
+# -------------------------- RECONNAISSANCE -----------------------------------
+
+# -------------------------- EXECUTION ----------------------------------------
+
+printinfo "Fetching validator indices from prysm-validator wallet..."
+validator_indices=$(sudo -u "$prysm_validator_user" "$prysm_validator_bin" accounts list \
+	--wallet-dir="$prysm_validator_wallet_dir" \
+	--wallet-password-file="$prysm_validator_wallet_password_file" \
+	--list-validator-indices \
+	--accept-terms-of-use \
+	--${ethereum_network} 2>/dev/null |
+	tee >(cat 1>&2) |
+	awk 'NR>1 {print $2}')
+validator_indices_csv="${validator_indices//$'\n'/,}"
+
+printinfo "Querying beacon chain for validator statuses..."
+api_url="http://localhost:3500/eth/v1/beacon/states/head/validators?id=$validator_indices_csv"
+printinfo "curl -X 'GET' \"$api_url\" -H 'accept: application/json' --no-progress-meter"
+curl -X 'GET' "$api_url" -H 'accept: application/json' --no-progress-meter | 
+	tee >(cat 1>&2) |
+	jq '.data' > "$_outfile"
+	
+# -------------------------- POSTCONDITIONS -----------------------------------
--- tools/export.sh
+++ tools/export.sh
@@ -32,22 +32,13 @@ press_any_key_to_continue
 assert_not_on_node_server
 assert_sudo
 
-check_is_defined ethereum_network
-check_is_defined dist_dirname
-
 check_is_valid_port node_server_ssh_port
 check_is_defined node_server_username
 check_is_defined node_server_hostname
 
-check_is_defined prysm_validator_wallet_dir
-check_is_defined prysm_validator_wallet_password_file
-check_is_defined prysm_validator_user
-
+check_is_defined dist_dirname
 check_is_defined client_pc_usb_data_drive
 check_is_defined validator_statuses_json
-
-usb_scripts_dir="$client_pc_usb_data_drive/$dist_dirname"
-check_directory_exists --sudo usb_scripts_dir
 
 print_failed_checks --error || exit
 
@@ -60,18 +51,17 @@ reset_checks
 check_file_does_not_exist --sudo validator_statuses_json
 if ! print_failed_checks --warn; then
 	continue_or_exit 1 "Overwrite?"
+	sudo rm -rf --interactive=never "$validator_statuses_json" >/dev/null
 	printf '\n'
-	# sudo rm -rf --interactive=never "$validator_statuses_json" 
 fi
 
 # -------------------------- EXECUTION ----------------------------------------
 
-assert_sudo
 set -e
+assert_sudo
 trap 'on_err_retry' ERR
 
-# create tempfiles
-printinfo "Creating remote tempfile..."
+# create local & remote temp files
 remote_temp_file="$(ssh -p $node_server_ssh_port $node_server_ssh_endpoint "echo \"\$(mktemp)\"")"
 local_temp_file="$(mktemp)"
 
@@ -84,13 +74,13 @@ print_failed_checks --error
 function on_exit() {
 	printinfo -n "Cleaning up..."
 
-	# delete remote tempfile if it exists
+	# delete remote temp file if it exists
 	ssh -p $node_server_ssh_port $node_server_ssh_endpoint "
 		temp_file=\"$remote_temp_file\"
 		[[ -f \$temp_file ]] && rm -rf --interactive=never \"\$temp_file\" &>/dev/null
 	"
 
-	# delete local tempfile if it exists
+	# delete local temp file if it exists
 	if [[ -f $local_temp_file ]]; then
 		rm -rf --interactive=never "$local_temp_file" &>/dev/null
 	fi
@@ -99,45 +89,16 @@ function on_exit() {
 }
 trap 'on_exit' EXIT
 
-# gather info about the validator server-side
+# run the remote getter script
 ssh -p $node_server_ssh_port $node_server_ssh_endpoint -t "
-	set -e
-	source \"\$HOME/$dist_dirname/common.sh\"
-	set_env
-	printinfo \"Logged into node server.\"
-	assert_sudo
-
-	reset_checks
-	temp_file=\"$remote_temp_file\"
-	check_file_exists --sudo temp_file
-	check_executable_exists --sudo prysm_validator_bin
-	print_failed_checks --error
-
-	printinfo "Fetching validator indices..."
-	sudo -u \"\$prysm_validator_user\" \"\$prysm_validator_bin\" accounts list \\
-		--wallet-dir=\"\$prysm_validator_wallet_dir\" \\
-		--wallet-password-file=\"\$prysm_validator_wallet_password_file\" \\
-		--list-validator-indices \\
-		--accept-terms-of-use \\
-		--\${ethereum_network} 2>/dev/null | tee -a \"\$temp_file\"
-	
-	printinfo \"Querying beacon chain for status of each validator...\"
-	validator_indices=\"\$(awk 'NR>1 {print \$2}' \"\$temp_file\")\"
-	validator_indices_csv=\"\${validator_indices//\$'\\n'/,}\"
-	api_url=\"http://localhost:3500/eth/v1/beacon/states/head/validators?id=\$validator_indices_csv\"
-	echo \"curl -X 'GET' \\\"\$api_url\\\" -H 'accept: application/json'\"
-	curl -X 'GET' \"\$api_url\" -H 'accept: application/json' > \"\$temp_file\"
+	\"\$HOME/$dist_dirname/get-validator-statuses.sh\" \"$remote_temp_file\"
 "
 
-# copy the gathered server-side info to local tempfile
-printinfo "Copying validator statuses to local tempfile..."
+printinfo "Transferring remote output to local 'DATA' drive..."
 rsync -avh -e "ssh -p $node_server_ssh_port" \
 	--progress \
 	"${node_server_ssh_endpoint}:${remote_temp_file}" "$local_temp_file"
-
-# prettify the tempfile and move it to local 'DATA' drive
-printinfo "Transforming JSON and moving to 'DATA' drive..."
-jq '.data' "$local_temp_file" | sudo tee "$validator_statuses_json"
+sudo mv -fv "$local_temp_file" "$validator_statuses_json"
 
 # -------------------------- POSTCONDITIONS -----------------------------------
 
@@ -147,6 +108,6 @@ print_failed_checks --error
 
 cat <<-EOF
 
-Validator statuses exported.
-To view file: ${theme_command}less -R "$validator_statuses_json"${color_reset}
+Saved as:
+${theme_command}$validator_statuses_json${color_reset}
 EOF
