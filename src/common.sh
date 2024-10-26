c176a1ce3af65711f0bcefb4a1b5eb48fef757a3
CoeJoder <CoeJoder@users.noreply.github.com>
2024-10-26 02:35:54 -0700

    Fix check_is_defined() arg passing

--- src/common.sh
+++ src/common.sh
@@ -666,7 +666,7 @@ function _check_is_defined() {
 
 # non-predicate (avoids triggering errexit)
 function check_is_defined() {
-	_check_is_defined && true
+	_check_is_defined "$1" && true
 }
 
 function check_argument_not_missing() {
