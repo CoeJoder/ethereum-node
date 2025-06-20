ddfa1cc97abc92ee38a85829c56cf18cf208a0e6
CoeJoder <CoeJoder@users.noreply.github.com>
2025-06-20 03:36:11 -0700

    Integrate Astro Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from "astro/config";
+
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    title: "ethereum-node docs",
+    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/CoeJoder/ethereum-node' }],
+    sidebar: [
+      {
+        label: 'Guides',
+        autogenerate: { directory: 'guides' },
+      },
+      {
+        label: 'Reference',
+        autogenerate: { directory: 'reference' },
+      },
+    ],
+  })]
+});
