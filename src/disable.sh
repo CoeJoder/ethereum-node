a2eb88618b380cd64157d447c60be936feb54185
CoeJoder <CoeJoder@users.noreply.github.com>
2025-06-29 18:06:21 -0700

    Add MEV-Boost to `disable` script

--- src/disable.sh
+++ src/disable.sh
@@ -2,7 +2,7 @@
 
 # disable.sh
 #
-# Stops & disables all node services (geth, prysm-beacon, prysm-validator).
+# Stops & disables all node services (geth, prysm-beacon, prysm-validator, MEV-Boost).
 #
 # Meant to be run on the node server.
 
@@ -21,9 +21,10 @@ assert_sudo
 
 reset_checks
 
-check_is_defined geth_unit_file
-check_is_defined prysm_beacon_unit_file
-check_is_defined prysm_validator_unit_file
+check_file_exists --sudo geth_unit_file
+check_file_exists --sudo prysm_beacon_unit_file
+check_file_exists --sudo prysm_validator_unit_file
+check_file_exists --sudo mevboost_unit_file
 
 print_failed_checks --error
 
@@ -42,7 +43,7 @@ echo -en "${color_reset}"
 # -------------------------- PREAMBLE -----------------------------------------
 
 cat <<EOF
-Stops & disables all node services (geth, prysm-beacon, prysm-validator).
+Stops & disables all node services (geth, prysm-beacon, prysm-validator, MEV-Boost).
 EOF
 press_any_key_to_continue
 
@@ -53,6 +54,7 @@ sudo systemctl daemon-reload
 _stop_geth=false
 _stop_prysmbeacon=false
 _stop_prysmvalidator=false
+_stop_mevboost=false
 
 reset_checks
 check_is_service_installed geth_unit_file
@@ -72,6 +74,12 @@ if ! has_failed_checks; then
 	_stop_prysmvalidator=true
 fi
 
+reset_checks
+check_is_service_installed mevboost_unit_file
+if ! has_failed_checks; then
+	_stop_mevboost=true
+fi
+
 # -------------------------- EXECUTION ----------------------------------------
 
 if [[ $_stop_geth == true ]]; then
@@ -84,6 +92,10 @@ fi
 
 if [[ $_stop_prysmvalidator == true ]]; then
 	disable_service "$prysm_validator_unit_file"
+fi
+
+if [[ $_stop_mevboost == true ]]; then
+	disable_service "$mevboost_unit_file"
 fi
 
 # -------------------------- POSTCONDITIONS -----------------------------------
