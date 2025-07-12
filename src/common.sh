8a85fba638ec90be00d13233c70e0fe7acc06ac7
CoeJoder <CoeJoder@users.noreply.github.com>
2025-07-12 07:32:53 -0700

    Update `choose_from_menu` implementation

--- src/common.sh
+++ src/common.sh
@@ -696,33 +696,28 @@ function press_any_key_to_continue() {
 # source: https://askubuntu.com/a/1386907
 # (with minor syntax changes to fix vscode syntax highlighting & linting)
 function choose_from_menu() {
-	local prompt="$1" outvar="$2" options cur count index esc
-	shift
-	shift
-	options=("$@")
-	cur=0
-	count=${#options[@]}
-	index=0
+	local -r prompt="$1" outvar="$2" options=("${@:3}")
+	local cur=0 count=${#options[@]} index=0 esc
 	esc=$(echo -en "\e") # cache ESC as test doesn't allow esc codes
 	printf "%s\n" "$prompt"
 	while true; do
 		# list all options (option list is zero-based)
 		index=0
 		for o in "${options[@]}"; do
-			if [ "$index" == "$cur" ]; then
+			if ((index == cur)); then
 				echo -e " >\e[7m$o\e[0m" # mark & highlight the current option
 			else
 				echo "  $o"
 			fi
-			index=$((index + 1))
+			((index++))
 		done
 		IFS= read -rs -n3 key             # wait for user to key in arrows or ENTER
 		if [[ $key == "${esc}[A" ]]; then # up arrow
-			cur=$((cur - 1))
-			[ "$cur" -lt 0 ] && cur=0
+			((cur--))
+			((cur < 0)) && ((cur = 0))
 		elif [[ $key == "${esc}[B" ]]; then # down arrow
-			cur=$((cur + 1))
-			[ "$cur" -ge "$count" ] && cur=$((count - 1))
+			((cur++))
+			((cur >= count)) && ((cur = count - 1))
 		elif [[ $key == "" ]]; then # nothing, i.e the read delimiter - ENTER
 			break
 		fi
